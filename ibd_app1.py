d6b067f58203da52500661850affed51b6a8adad
gmaheshkumar15 <gmaheshk85@gmail.com>
2025-11-22 15:52:20 +0530

    Add intro paragraph separator in ibd_app1.py

--- ibd_app1.py
+++ ibd_app1.py
@@ -80,6 +80,10 @@ with col_logo_right:
     st.markdown('<img src="https://raw.githubusercontent.com/gmaheshkumar15/ibd_similarity_score_-predictor/main/dmch.jpeg" class="logo-right">', unsafe_allow_html=True)
     st.markdown('<div class="institute-name">Dayanand Medical College and Hospital Ludhiana</div>', unsafe_allow_html=True)
 
+
+
+
+
 # -----------------------------
 # Intro Paragraph
 # -----------------------------
